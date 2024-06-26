7c9f582d0014c766755e90dbe7a7487284e1f770
Marina Khadieva <92573868+MariKhad@users.noreply.github.com>
2024-06-26 12:37:11 +0300

    more checks

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
   operandInput1.addEventListener("input", validateInput);
   operandInput2.addEventListener("input", validateInput);
 
+  //input type="nubmer" не работает в firefox
   function validateInput(event) {
-    const value = event.target.value;
     event.target.value = value.replace(/[^0-9.-]/g, "");
-    if (value.indexOf("-") > 0) {
-      value = value.replace(/-/g, "");
+    if (event.target.value.indexOf("-") > 0) {
+      event.target.value = value.replace(/-/g, "");
     }
-    const parts = value.split(".");
+    const parts = event.target.value.split(".");
     if (parts.length > 2) {
-      value = parts.shift() + "." + parts.join("");
+      event.target.value = parts.shift() + "." + parts.join("");
     }
   }
 
@@ -43,6 +43,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (operator in OPERATORS) {
       result.textContent = OPERATORS[operator];
+      operandInput1.value = "";
+      operandInput2.value = "";
     } else {
       result.textContent = "Неподдерживаемая операция";
     }
